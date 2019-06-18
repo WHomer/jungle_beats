3888f9b1a54d01bdf564cbee02a54ab2d6184efc
William Homer <billyhomeriii@gmail.com>
2019-06-18 14:41:34 -0500

    resolve itteration 1

---
+++ test/node_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require './lib/node'
+
+class NodeTest < MiniTest::Test
+  def setup
+    @node = Node.new("plop")
+  end
+
+  def test_does_it_exist
+    assert_instance_of Node, @node
+  end
+
+  def test_test_instance_variables
+    assert "plop", @node.data
+    refute @node.next_node
+  end
+  
+
+end
